e5a3255d858d029d213af88b9c3c8f6decd5bfad
HEMANTH KS <96233075+HemanthKs123@users.noreply.github.com>
2025-06-18 20:35:54 +0530

    Add files via upload

---
+++ types.h
@@ -0,0 +1,23 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+/* User defined types */
+
+typedef unsigned int uint;
+
+/* Status will be used in function return type */
+
+typedef enum
+{
+    e_success,		//0
+    e_failure		//1
+} Status;
+
+typedef enum
+{
+    e_encode,		//0
+    e_decode,		//1
+    e_unsupported	//2
+} OperationType;
+
+#endif
